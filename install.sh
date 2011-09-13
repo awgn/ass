f876da1267e93ebcb458065e8334eab3170c8d4c
Nicola Bonelli <nicola.bonelli@gmail.com>
2011-09-13 22:22:52 +0200

    added install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -x
+
+/usr/bin/g++ ass.cpp -std=c++0x -Wall -Wextra -o /usr/local/bin/ass 
+/usr/bin/g++ ass.hpp -std=c++0x -O0 -o /usr/local/include/ass.hpp.gch
+/bin/cp ass.hpp /usr/local/include/
+

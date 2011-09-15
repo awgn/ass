e3dbbd54cb107eb88f40dac42f8ea3d07f6a3b1c
Nicola Bonelli <nicola.bonelli@gmail.com>
2011-09-16 00:55:24 +0200

    added license

--- ass.hs
+++ ass.hs
@@ -1,3 +1,20 @@
+--
+-- Copyright (c) 2011 Bonelli Nicola <bonelli@antifork.org>
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+--
 -- ass: C++ code ass'istant
 
 import Char
@@ -62,7 +79,6 @@ main = do
     let testCmd = "/tmp/runme " ++ ( unwords $ getTestArgs args ) 
 
     -- parse the snippet.
-
     source <- hGetContents stdin >>= toCodeLine
     let (translation',main') = parseSource translation main source
 

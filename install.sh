704d52f3ccb9c401a702a1af5910575f187a541f
Nicola Bonelli <nicola.bonelli@gmail.com>
2012-02-11 17:10:45 +0100

    Script updated

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e -x
 
-/usr/bin/ghc -Wall ass.hs -o /usr/local/bin/ass
+/usr/bin/ghc -O -Wall ass.hs -o /usr/local/bin/ass
 
 /usr/bin/g++ ass.hpp -std=c++0x -O0 -o /usr/local/include/ass.hpp.gch
 

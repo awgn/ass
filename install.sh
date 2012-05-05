1f0cab32bae04744792419b5cf8a608ba617bb02
Nicola Bonelli <nicola.bonelli@gmail.com>
2012-05-05 21:04:47 +0200

    Renamed Ass and Gen with capital letter

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e -x
 
-/usr/bin/ghc -O -Wall ass.hs -o /usr/local/bin/ass
-/usr/bin/ghc -O -Wall gen.hs -o /usr/local/bin/gen
+/usr/bin/ghc -O -Wall Ass.hs -o /usr/local/bin/ass
+/usr/bin/ghc -O -Wall Gen.hs -o /usr/local/bin/gen
 
 /usr/bin/g++ ass.hpp -std=c++0x -O0 -D_GLIBCXX_DEBUG -Wall -Wextra -o /usr/local/include/ass.hpp.gch
 

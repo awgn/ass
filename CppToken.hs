f1c4141fe80a2c1f9ce1f6820eacf5ceb4a3c136
Nicola Bonelli <nicola.bonelli@gmail.com>
2012-05-12 21:40:33 +0200

    Updated: removed warnings

--- CppToken.hs
+++ CppToken.hs
@@ -17,20 +17,9 @@
 -- ass: C++11 code ass'istant 
 
 
-module CppToken where
+module CppToken(Token(..), tokens)  where
       
 import Data.Char
-import Data.List
-
--- Requred for Header Name Token
---
-data PreprocState = Null | Hash | Include
-                    deriving (Show, Eq)
-
-nextState :: String -> PreprocState -> PreprocState
-nextState "#" Null       = Hash
-nextState "include" Hash = Include
-nextState _   _          = Null
 
 
 -- Tokenize the source code in a list of Token
@@ -46,27 +35,36 @@ data Token = NullToken  |
              OperOrPunct { toString :: String }
                 deriving (Show, Read)
 
-
 tokens :: String -> [Token]
 tokens xs = getTokens xs Null  
 
 
+-- Requred for Header Name Token
+--
+data PreprocState = Null | Hash | Include
+                    deriving (Show, Eq)
+
+nextState :: String -> PreprocState -> PreprocState
+nextState "#" Null       = Hash
+nextState "include" Hash = Include
+nextState _   _          = Null
+
 getTokens :: String -> PreprocState -> [Token]
-getTokens [] state = []
-getTokens xs state = token : getTokens ls (nextState (toString token) state) 
+getTokens [] _ = []
+getTokens xs state = token : getTokens ls (nextState (toString token) next) 
                         where (token, next) = runGetToken xs state
                               ls = dropWhile (\c -> c `elem` whitespace) $ drop (length $ toString token) xs                        
 
 runGetToken :: String -> PreprocState -> (Token, PreprocState)
 runGetToken []  s = (NullToken, s)
 runGetToken xs  s = case xs' of 
-                        (y:ys) | y == '0'       -> (getTokenNumber xs', s)
-                        (y:ys) | y == '"'       -> (getTokenString xs', s)
-                        (y:ys) | y == '\''      -> (getTokenChar xs', s)
-                        (y:ys) | (y == '<') && 
-                                 (s == Include) -> (getTokenHeaderName xs', s)
-                        (y:ys) | isIdentifier y -> (getTokenIdOrKeyword xs', s)
-                        otherwise               -> (getTokenOpOrPunct xs',s)
+                        (y:_) | y == '0'       -> (getTokenNumber xs', s)
+                        (y:_) | y == '"'       -> (getTokenString xs', s)
+                        (y:_) | y == '\''      -> (getTokenChar   xs', s)
+                        (y:_) | (y == '<') && 
+                                (s == Include) -> (getTokenHeaderName  xs', s)
+                        (y:_) | isIdentifier y -> (getTokenIdOrKeyword xs', s)
+                        _                      -> (getTokenOpOrPunct   xs', s)
                         where
                            xs' = dropWhile (\c -> c `elem` whitespace) xs
 
@@ -77,29 +75,33 @@ getTokenIdOrKeyword, getTokenNumber, getTokenHeaderName, getTokenString, getToke
 getTokenIdOrKeyword xs 
     | name `elem` keywords = Keyword name
     | otherwise            = Identifier name
-                            where name = takeWhile isIdentifier xs
+                where name = takeWhile isIdentifier xs
+
 getTokenNumber      xs = Number     (takeWhile isLiteralNum xs)
 getTokenHeaderName  xs = HeaderName (getLiteralDelim '<'  '>'  False xs)
 getTokenString      xs = TString    (getLiteralDelim '"'  '"'  False xs)
 getTokenChar        xs = TChar      (getLiteralDelim '\'' '\'' False xs)
 
 
-getTokenOpOrPunct (a:b:c:d:xs) 
-    | a:b:c:[d]`elem`oper4 = OperOrPunct (a:b:c:[d])
-    | a:b:[c] `elem` oper3 = OperOrPunct (a:b:[c])
-    | a:[b]   `elem` oper2 = OperOrPunct (a:[b])
-    | a       `elem` oper1 = OperOrPunct [a]
-getTokenOpOrPunct (a:b:c:xs) 
-    | a:b:[c] `elem` oper3 = OperOrPunct (a:b:[c])
-    | a:[b]   `elem` oper2 = OperOrPunct (a:[b])
-    | a       `elem` oper1 = OperOrPunct [a]
-getTokenOpOrPunct (a:b:xs) 
-    | a:[b]   `elem` oper2 = OperOrPunct (a:[b])
-    | a       `elem` oper1 = OperOrPunct [a]
-getTokenOpOrPunct (a:xs) 
-    | a       `elem` oper1 = OperOrPunct [a]
-    | otherwise = error "getTokenOpOrPunct"
-
+getTokenOpOrPunct (a:b:c:d:_) 
+    | a:b:c:[d] `elem` (oper_or_punct !! 3) = OperOrPunct (a:b:c:[d])
+    | a:b:[c]   `elem` (oper_or_punct !! 2) = OperOrPunct (a:b:[c])
+    | a:[b]     `elem` (oper_or_punct !! 1) = OperOrPunct (a:[b])
+    | a         `elem` (oper_or_punct !! 0 !! 0) = OperOrPunct [a]
+    | otherwise  = error "getTokenOpOrPunct"
+getTokenOpOrPunct (a:b:c:_) 
+    | a:b:[c]   `elem` (oper_or_punct !! 2) = OperOrPunct (a:b:[c])
+    | a:[b]     `elem` (oper_or_punct !! 1) = OperOrPunct (a:[b])
+    | a         `elem` (oper_or_punct !! 0 !! 0) = OperOrPunct [a]
+    | otherwise  = error "getTokenOpOrPunct"
+getTokenOpOrPunct (a:b:_) 
+    | a:[b]     `elem` (oper_or_punct !! 1) = OperOrPunct (a:[b])
+    | a         `elem` (oper_or_punct !! 0 !! 0) = OperOrPunct [a]
+    | otherwise  = error "getTokenOpOrPunct"
+getTokenOpOrPunct (a:_) 
+    | a         `elem` (oper_or_punct !! 0 !! 0) = OperOrPunct [a]
+    | otherwise  = error "getTokenOpOrPunct"
+getTokenOpOrPunct []  = error "getTokenOpOrPunct" 
 
 getLiteralDelim :: Char -> Char -> Bool -> String -> String
 getLiteralDelim _  _  _ []  = []
@@ -120,17 +122,18 @@ isIdentifier, isLiteralNum :: Char -> Bool
 isIdentifier = \c -> isAlphaNum c || c == '_'
 isLiteralNum = \c -> c `elem` "0123456789abcdefABCDEF.xXeEuUlL"
 
-whitespace = " \t\r\n"
+whitespace :: [Char]
+whitespace = " \t\r\n" 
 
-oper4 = [ "%:%:" ]
-oper3 = ["...", "<<=", ">>=", "->*"]
-oper2 = ["##", "<:", ":>", "<%", "%>", "%:", "::", ".*", 
-         "+=", "-=", "*=", "/=", "%=", "^=", "&=", "|=", 
-         "<<", ">>", ">=", "<=", "&&", "||", "==", "!=", 
-         "++", "--", "->" ]
-
-oper1 = "{}[]#();:?.+-*/%^&|~!=<>,"
-
+oper_or_punct :: [[[Char]]]
+oper_or_punct = [   [ "{}[]#();:?.+-*/%^&|~!=<>," ],
+                    ["##", "<:", ":>", "<%", "%>", "%:", "::", ".*", "+=",   
+                     "-=", "*=", "/=", "%=", "^=", "&=", "|=", "<<", ">>",   
+                     ">=", "<=", "&&", "||", "==", "!=", "++", "--", "->" ],      
+                    ["...", "<<=", ">>=", "->*"],   
+                    [ "%:%:" ]       
+                ]
+keywords :: [[Char]]
 keywords = ["alignas", "continue", "friend", "alignof", "decltype", "goto",
             "asm", "default", "if", "auto", "delete", "inline", "bool", "do", "int",
             "break", "double", "long", "case", "dynamic_cast", "mutable", "catch", "else", 
@@ -139,5 +142,8 @@ keywords = ["alignas", "continue", "friend", "alignof", "decltype", "goto",
             "constexpr", "float", "protected", "const_cast", "for", "public", "register", "true", 
             "reinterpret_cast", "try", "return", "typedef", "short", "typeid", "signed", "typename", 
             "sizeof", "union", "static", "unsigned", "static_assert", "using", "static_cast", "virtual", 
-            "struct", "void", "switch", "volatile", "template", "wchar_t", "this", "while", "thread_local", "throw"]
+            "struct", "void", "switch", "volatile", "template", "wchar_t", "this", "while", "thread_local", "throw",
+            "and", "and_eq", "bitand", "bitor", "compl", "not", "not_eq", "or", "or_eq", "xor", "xor_eq"]
+
+
 

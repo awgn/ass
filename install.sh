2ba7e3854b97417c2ff8b47f97469cccc9c96bb9
Nicola Bonelli <nicola.bonelli@gmail.com>
2012-08-11 10:43:22 +0200

    ass.hpp and ass-mt.hpp moved to includes directory

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ set -e -x
 /usr/bin/ghc -O -Wall Ass.hs -o /usr/local/bin/ass
 /usr/bin/ghc -O -Wall Gen.hs -o /usr/local/bin/gen
 
-/usr/bin/g++ ass.hpp    -std=c++0x -O0 -D_GLIBCXX_DEBUG -Wall -Wextra -o /usr/local/include/ass.hpp.gch
-/usr/bin/g++ ass-mt.hpp -std=c++0x -O0 -D_GLIBCXX_DEBUG -Wall -Wextra -o /usr/local/include/ass-mt.hpp.gch
+/usr/bin/g++ includes/ass.hpp    -std=c++0x -O0 -D_GLIBCXX_DEBUG -Wall -Wextra -o /usr/local/include/ass.hpp.gch
+/usr/bin/g++ includes/ass-mt.hpp -std=c++0x -O0 -D_GLIBCXX_DEBUG -Wall -Wextra -o /usr/local/include/ass-mt.hpp.gch
 
-/bin/cp ass.hpp /usr/local/include/
-/bin/cp ass-mt.hpp /usr/local/include/
+/bin/cp includes/ass.hpp /usr/local/include/
+/bin/cp includes/ass-mt.hpp /usr/local/include/
 

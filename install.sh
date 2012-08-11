612601eef172ae5f8711eed9375bc31101d99160
Nicola Bonelli <nicola.bonelli@gmail.com>
2012-08-11 10:31:23 +0200

    Added gen in install script

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 set -e -x
 
 /usr/bin/ghc -O -Wall Ass.hs -o /usr/local/bin/ass
+/usr/bin/ghc -O -Wall Gen.hs -o /usr/local/bin/gen
 
 /usr/bin/g++ ass.hpp    -std=c++0x -O0 -D_GLIBCXX_DEBUG -Wall -Wextra -o /usr/local/include/ass.hpp.gch
 /usr/bin/g++ ass-mt.hpp -std=c++0x -O0 -D_GLIBCXX_DEBUG -Wall -Wextra -o /usr/local/include/ass-mt.hpp.gch

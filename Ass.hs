966680b0eff709a067179ad2cdc2563645d65162
Nicola Bonelli <nicola.bonelli@gmail.com>
2013-04-15 11:40:14 +0200

    1.2.5: In Token, added lineno property

--- Ass.hs
+++ Ass.hs
@@ -66,7 +66,7 @@ compilerList = [
 
 banner, snippet, tmpDir,assrc, ass_history :: String 
 
-banner  = "ASSi, version 1.2.4 :? for help"
+banner  = "ASSi, version 1.2.5 :? for help"
 snippet     = "snippet" 
 tmpDir      =  "/tmp" 
 assrc       =  ".assrc"

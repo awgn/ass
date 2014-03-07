308231ffbb6465c6a21e5e7ed38a94a50d60777b
Nicola Bonelli <nicola.bonelli@gmail.com>
2014-03-07 18:04:36 +0100

    2.12 released.

--- src/Config.hs
+++ src/Config.hs
@@ -22,7 +22,7 @@ module Config where
 banner, snippet, assrc, ass_history :: String
 tmpDir, includeAssDir :: FilePath
 
-banner          = "ASSi, version 2.11"
+banner          = "ASSi, version 2.12"
 
 includeAssDir   =  "/usr/local/include/ass"
 installDir      =  "/usr/local/bin/"
